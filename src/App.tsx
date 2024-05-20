0ff2a91b5876af71247ba71703fe45dc2d85786b
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2024-05-21 00:51:00 +0100

    project init with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto border border-gray-400 p-5 rounded-md">
+        <p className="text-lg text-center">dark mode intro</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
